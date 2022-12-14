72b25f0579504204d5102e282c8d1cd7ab384cc4
Junaid Malik <junmalik109@gmail.com>
2022-12-14 12:53:12 +0000

    updated app entry file

--- components/Exhanges.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Exhanges = () => {
-  return (
-    <div>Exhanges</div>
-  )
-}
-
-export default Exhanges
--- src/App.js
+++ src/App.js
@@ -1,29 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
-import { Layout, Typography, Space} from 'antd';
-import { Navbar, Footer } from './components/Navbar';
-function App() {
-  return (
-    <div className="app">
-      <header className="navbar">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Green Earth Token
-        </p>
-   
-      </header>
-    <div className='main'>
+import { Layout, Typography, Space } from 'antd';
 
+import { Exchanges, HomePage, News, Cryptocurrencies, CryptoDetails, Navbar } from './components';
+import './App.css';
+
+const App = () => (
+  <div className="app">
+    <div className="navbar">
+      <Navbar />
     </div>
-
-    <div className='footer'>
-
+    <div className="main">
+      <Layout>
+        <div className="routes">
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route exact path="/exchanges">
+              <Exchanges />
+            </Route>
+            <Route exact path="/cryptocurrencies">
+              <Cryptocurrencies />
+            </Route>
+            <Route exact path="/crypto/:coinId">
+              <CryptoDetails />
+            </Route>
+            <Route exact path="/news">
+              <News />
+            </Route>
+          </Switch>
+        </div>
+      </Layout>
+      <div className="footer">
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+          <Link to="https://junaidmalik.org">
+            Junaid Malik.
+          </Link> <br />
+          
+        </Typography.Title>
+        <Space>
+          <Link to="/">Home</Link>
+          <Link to="/exchanges">Exchanges</Link>
+          <Link to="/news">News</Link>
+        </Space>
+      </div>
     </div>
-    </div>
-
-  );
-}
+  </div>
+);
 
 export default App;
